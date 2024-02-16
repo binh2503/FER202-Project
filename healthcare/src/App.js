5349acdce227cb328fb213fe3c42142647ce3135
binh2503 <binhdghe170773@fpt.edu.vn>
2024-02-16 10:12:08 +0700

    pre commit

--- healthcare/src/App.js
+++ healthcare/src/App.js
@@ -4,6 +4,7 @@ import ForgotPassword from "./components/ForgotPassword";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage";
 function App() {
   return (
     // <div className="w-screen h-screen">
@@ -11,18 +12,19 @@ function App() {
     //   {/* <Signup /> */}
     //   {/* <ForgotPassword /> */}
     // </div>
-    
-      <div className="w-screen h-screen">
-      <BrowserRouter>
-          <Routes>
-            <Route path='/signup' element={<Signup />} > </Route>
-            <Route path='/login' element={<Login />} > </Route>
-            <Route path='/forgotpassword' element={<ForgotPassword/>}></Route>
-          </Routes>
 
-        </BrowserRouter>
-      </div>
-    
+    <div className="w-screen h-screen">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Signup />} > </Route>
+          <Route path='/login' element={<Login />} > </Route>
+          <Route path='/forgotpassword' element={<ForgotPassword />}></Route>
+          <Route path='/homepage' element={<Homepage />}></Route>
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+
   );
 }
 
---
+++ healthcare/src/components/Header.js
@@ -0,0 +1,31 @@
+import { UilUser } from '@iconscout/react-unicons'
+export default function Header() {
+    return (
+        <div className="w-screen h-auto bg-white flex justify-center">
+            <div className="flex justify-between h-[100px] w-4/5">
+                <div className="h-full flex">
+                    <a className="h-full" href="/homepage"><img className="h-full" src="https://media.istockphoto.com/id/1321617070/vector/health-medical-logo.jpg?s=612x612&w=0&k=20&c=sus8vhG3c__vCdvOBLDhuf2vPUgIAudIAeUBApU_7Ew=" /></a>
+                    <div className="h-full flex items-center ml-[-20px]">
+                        <a href="/homepage">
+                            <p className="font-mono text-[#0493B3] text-[35px] font-bold">Healthcare</p>
+                        </a>
+                    </div>
+                </div>
+                <div className="h-full flex items-center">
+                    <div className="gap-[50px] flex">
+                        <a className="text-[18px] font-mono font-bold" href="#">Home</a>
+                        <a className="text-[18px] font-mono font-bold" href="#">About</a>
+                        <a className="text-[18px] font-mono font-bold" href="#">Specialist</a>
+                        <a className="text-[18px] font-mono font-bold" href="#">Doctors</a>
+                        <a className="text-[18px] font-mono font-bold" href="#">Contact</a>
+                    </div>
+                </div>
+                <div className="h-full flex items-center">
+                    <a href="/profile">
+                        <button className="h-[40px] w-[40px] bg-[#0872BB] flex justify-center items-center text-white font-mono font-bold rounded-[30px]"><UilUser /></button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
