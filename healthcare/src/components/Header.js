585441cd3fc6ace20c3626157c0d9f3a72e7bd78
quanganh3183 <quanganh3183@gmail.com>
2024-03-11 19:50:50 +0700

    Update Notification

--- healthcare/src/components/Header.js
+++ healthcare/src/components/Header.js
@@ -1,4 +1,4 @@
-import { UilUser, UilSignout } from '@iconscout/react-unicons'
+import { UilUser, UilSignout, UilBell } from '@iconscout/react-unicons'
 import Signup from './Signup';
 export default function Header() {
     return (
@@ -22,13 +22,19 @@ export default function Header() {
                     </div>
                 </div>
                 <div className="h-full flex items-center gap-[20px]">
+                    <div className='relative'>
+                        <button className="h-[40px] w-[40px] bg-[#0872BB] flex justify-center items-center text-white font-mono font-bold rounded-[30px]"><UilBell /></button>
+                        <div className='absolute border w-[18px] h-[18px] bg-white rounded-[10px] top-[25px] left-[25px] flex items-center justify-center'>
+                            <p className='font-bold font-mono text-[#FF0000] text-[14px]' >1</p>
+                        </div>
+
+                    </div>
                     <a href="/profile">
                         <button className="h-[40px] w-[40px] bg-[#0872BB] flex justify-center items-center text-white font-mono font-bold rounded-[30px]"><UilUser /></button>
                     </a>
                     <a href="/">
                         <button className="h-[40px] w-[40px] bg-[#0872BB] flex justify-center items-center text-white font-mono font-bold rounded-[30px]"><UilSignout /></button>
                     </a>
-
                 </div>
             </div>
         </div>
